5978040245c99e2a3dd4ff2d3f16300166998664
Chamiko@foxmail.com <Chamiko@foxmail.com>
2025-04-07 01:48:15 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -4,9 +4,7 @@ import json
 app = Flask(__name__)
 
 VERSIONS = {
-    "ExpressCommand": "20250404",
-    "test": "114514",
-    "StoreOffline": "20250404"
+    "ExpressCommand": {"version": "20250404", "msg": "版本已更新（20250407）：1.修复了极兔修改位置错误的BUG；2.修复了自动复制不全仍发出的BUG"}
 }
 
 @app.route('/get_version', methods=['GET'])
@@ -16,9 +14,7 @@ def get_version():
         return jsonify({"error": "Missing 'name' parameter"}), 400
     
     if app_name in VERSIONS:  # 直接使用原始的 VERSIONS 字典
-        return jsonify({
-            "version": VERSIONS[app_name]  # 直接从字典中获取值
-        })
+        return jsonify(VERSIONS[app_name])  # 直接从字典中获取值
     else:
         return jsonify({"error": f"App '{app_name}' not found"}), 404
 
